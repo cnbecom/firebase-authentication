99af3ff143df03f4d9ad6ba5627c83e5f56a8787
Christopher Becom <cnbecom@gmail.com>
2018-06-13 22:09:39 -0400

    Added initial Email/Password integration.

--- FirebaseAuthentication/FirebaseAuthentication/ViewController.swift
+++ FirebaseAuthentication/FirebaseAuthentication/ViewController.swift
@@ -6,19 +6,37 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    var handle: AuthStateDidChangeListenerHandle?
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            // ...
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
